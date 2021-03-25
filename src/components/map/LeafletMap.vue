3897e39c81489e004621e71c1ef61942ad79e759
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-03-25 15:11:07 +0100

    add prettier rules and again scss file for LeatlefMap

--- src/components/map/LeafletMap.vue
+++ src/components/map/LeafletMap.vue
@@ -1,16 +1,19 @@
 <template>
-	<div id="mapContainer">
-		<l-map
-			style="height: 80%; width: 100%"
-			:zoom="zoom"
-			:center="center"
-			@update:zoom="zoomUpdated"
-			@update:center="centerUpdated"
-			@update:bounds="boundsUpdated"
-		>
-			<l-tile-layer :url="url"></l-tile-layer>
-		</l-map>
-	</div>
+  <div class="map">
+    <h1>World Map</h1>
+    <div id="mapContainer">
+      <l-map
+        style="height: 80%; width: 100%"
+        :zoom="zoom"
+        :center="center"
+        @update:zoom="zoomUpdated"
+        @update:center="centerUpdated"
+        @update:bounds="boundsUpdated"
+      >
+        <l-tile-layer :url="url"></l-tile-layer>
+      </l-map>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,36 +21,31 @@ import "leaflet/dist/leaflet.css";
 import { LMap, LTileLayer } from "vue2-leaflet";
 
 export default {
-	name: "LeafletMap",
-	components: {
-		LMap,
-		LTileLayer,
-	},
-	data() {
-		return {
-			url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-			zoom: 3,
-			center: [0, 0],
-			bounds: null,
-		};
-	},
-	methods: {
-		zoomUpdated(zoom) {
-			this.zoom = zoom;
-		},
-		centerUpdated(center) {
-			this.center = center;
-		},
-		boundsUpdated(bounds) {
-			this.bounds = bounds;
-		},
-	},
+  name: "LeafletMap",
+  components: {
+    LMap,
+    LTileLayer
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      zoom: 1,
+      center: [0, 0],
+      bounds: null
+    };
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    }
+  }
 };
 </script>
 
-<style scoped>
-#mapContainer {
-	width: 80vw;
-	height: 100vh;
-}
-</style>
+<style src="./LeafletMap.css" scoped />
